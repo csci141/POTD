39021cc7e7661942b0e2fe5ed499b95491e712ad
Caroline Hardin <hardinc3@wwu.edu>
2026-03-04 17:13:40 -0800

    added more notes re timeout

--- skel/P19_sieve_test.py
+++ skel/P19_sieve_test.py
@@ -67,3 +67,6 @@ def test_specific_numbers(n, m, expected):
     assert result[m] == expected
 
 pytest.main(["P19_sieve_test.py", "-p", "no:faulthandler", "-vv", "-s", "--showlocals"])
+#to prevent it from running forever if you have an infinite loop, you can set it to time out.
+#You'll need to install pytest timeout : see https://pypi.org/project/pytest-timeout/
+#pytest.main(["P19_sieve_test.py", "-p", "no:faulthandler", "-vv", "-s", "--showlocals", "--timeout", "120"])
